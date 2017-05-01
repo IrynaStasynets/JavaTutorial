b39ffec3bcc613c072a89738ea5c7b36f6d923af
Iryna Stasynets <iryna.stasynets@gmail.com>
2017-05-01 14:51:44 +0300

    Initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.printHelloWorld();
+    }
+}
---
+++ HelloWorld/src/Printer.java
@@ -0,0 +1,7 @@
+public class Printer {
+
+    public void printHelloWorld() {
+        String hello = "Rakamakafo" + 5 + 1;
+        System.out.println(hello);
+    }
+}
